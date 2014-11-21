9a93acea9a60ac1e4ac969fea0c6e605c29c5da5
manuel <insphare@gmail.com>
2014-11-21 15:03:14 +0100

    Initial commit

--- htdocs/index.php
+++ htdocs/index.php
@@ -4,7 +4,7 @@ define('DS', DIRECTORY_SEPARATOR);
 
 // quick and dirty autoloader
 spl_autoload_register(function ($class) {
-	$libFilePath = dirname(__FILE__) . DS . 'lib/' . $class . '.php';
+	$libFilePath = dirname(dirname(__FILE__)) . DS . 'lib/' . $class . '.php';
 
 	if (file_exists($libFilePath)) {
 		require_once $libFilePath;
@@ -16,7 +16,7 @@ $pass = isset($_POST['password']) ? $_POST['password'] : null;
 
 Config::set(Config::KEY_USERNAME, $user);
 Config::set(Config::KEY_PASSWORD, $pass);
-Config::set(Config::KEY_JIRA_HOST, file_get_contents(dirname(__FILE__).DS.'host'));
+Config::set(Config::KEY_JIRA_HOST, file_get_contents(dirname(dirname(__FILE__)).DS.'host'));
 
 $jira = new Jira();
 //@todo test+view+design
