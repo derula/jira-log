774a7873de886c1353d4ca0af84b6ed8665efd3a
derula <derula@uglyhorst.de>
2014-12-16 21:09:17 +0100

    CLI Server in single php file

---
+++ htdocs/server.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * About this script
+ *
+ * This script sets up a simple cli-server. This is ONLY for testing purposes /
+ * personal use. No warranty is provided. There were no special precautions met
+ * in regards to security. Under no circumstances should you use this in a live
+ * environment. No support will be provided.
+ *
+ * To run the server, just execute this script with PHP.
+ * The server can be accessed through localhost:8000.
+ */
+
+$dir = __DIR__;
+$file = __FILE__;
+
+// Start the server
+if (PHP_SAPI === 'cli') {
+	$descs = array (
+		0 => array ('file', 'php://stdin', 'r'),
+		1 => array ('file', 'php://stdout', 'w'),
+		2 => array ('file', 'php://stderr', 'w')
+	);
+	$proc = proc_open ("php -S localhost:8000 -t $dir $file", $descs, $fp);
+	if (is_resource($proc)) {
+		exit(proc_close($proc));
+	}
+}
+
+// Do the routing magic
+if (is_file($dir.$_SERVER['REQUEST_URI'])) {
+	return false;
+}
+else {
+	require($dir.DIRECTORY_SEPARATOR.'dispatcher.php');
+}
